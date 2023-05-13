3e07a783c435e3cfc094ce627d16ff90042bd625
viryl scott <viryl1515@gmail.com>
2023-05-13 15:20:55 +0100

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <script setup>
 import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
-import { useProductStore } from "./stores/ProductStore";
+import { useProductStore } from "@/stores/ProductStore";
+
+import {useCartStore} from "@/stores/CartStore";
 
 const productStore = useProductStore();
 
+const cartStore = useCartStore();
+
 productStore.fill();
+
+// const addToCart = (count, product) => {
+//   count = parseInt(count);
+
+//   cartStore.$patch(state => {
+//     for (let index = 0; index < count; index++) {
+//       state.items.push(product)
+//     }
+//   })
+// }
 
 </script>
 
@@ -17,6 +31,7 @@ productStore.fill();
         v-for="product in productStore.products"
         :key="product.name"
         :product="product"
+        @addToCart="cartStore.addItems($event, product)"
       />
     </ul>
   </div>
---
+++ src/stores/CartStore.js
@@ -0,0 +1,31 @@
+import { defineStore } from 'pinia';
+
+import { groupBy, sumBy } from 'lodash';
+
+export const useCartStore = defineStore("CartStore", {
+    state: () => {
+        return {
+            items: []
+        }
+    },
+
+    getters: {
+        count: (state) => state.items.length,
+        isEmpty: (state) => state.count === 0,
+
+        grouped: (state) => groupBy(state.items, (item) => item.name),
+
+        groupCount: (state) => (name) => state.grouped[name].length,
+        total: (state) => sumBy(state.items, 'price'),
+    },
+
+    actions: {
+        addItems(count, item) {
+            count = parseInt(count);
+
+            for (let index = 0; index < count; index++) {
+                this.items.push({...item})
+              }
+        }
+    }
+})
