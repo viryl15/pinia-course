3642e7340a71282fede3199fa502fc87d7ebf898
viryl scott <viryl1515@gmail.com>
2023-05-14 11:40:09 +0100

    wip

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,6 +1,24 @@
-<script setup>
+<script>
 // imports
+import { useAuthUserStore } from '@/stores/AuthUserStore';
+import { mapState, mapActions } from "pinia";
 import CartWidget from './CartWidget.vue';
+
+export default {
+  components: {
+    CartWidget
+  },
+  computed: {
+    ...mapState(useAuthUserStore, {
+      user: (store) => `Hello ${store.username}`,
+    }),
+  },
+  methods: {
+    ...mapActions(useAuthUserStore, {
+      toTwitter: "visitTwitterProfile"
+    }),
+  }
+}
 </script>
 
 <template>
@@ -9,6 +27,9 @@ import CartWidget from './CartWidget.vue';
     style="background-image: url('/images/double-bubble-outline.png')"
   >
     <h1 class="text-4xl text-gray-700 font-bold">The Pineapple Stand</h1>
-    <CartWidget/>
+    <div>
+      <span class="mr-5" @click="toTwitter">{{ user }}</span>
+      <CartWidget class="inline-block"/>
+    </div>
   </header>
 </template>
--- src/stores/AuthUserStore.js
+++ src/stores/AuthUserStore.js
@@ -3,7 +3,13 @@ import { defineStore } from "pinia";
 export const useAuthUserStore = defineStore("AuthUserStore", {
     state: () => {
         return {
-            username: "viryl15"
+            username: "viryl3"
+        }
+    },
+
+    actions: {
+        visitTwitterProfile(){
+            window.open(`https://twitter.com/${this.username}`, "_blank");
         }
     }
 })
